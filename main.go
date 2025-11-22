096698bd44c2cde60bfc8e7e6e2dcef3ba56b07e
shahadathhs <shahadathhossensajib732@gmail.com>
2025-11-23 02:14:32 +0600

    chore: update swagger doc for base route

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /
 func main() {
 	mux := http.NewServeMux()
